59d9ae099cf61a2f541876293ba61c7a4f8e005c
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2020-06-13 15:55:36 +0530

    Update TagManager.php

--- src/Itzdvbravo/Tags/TagManager.php
+++ src/Itzdvbravo/Tags/TagManager.php
@@ -11,7 +11,7 @@ use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat;
-use Itzdvbravo\Tags\libs\jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\SimpleForm;
 
 class TagManager extends PluginBase{
     public static $config;
